4046a61796705b40abec1efab939909eb521079a
Florian Klein <florian.klein@free.fr>
2022-06-04 12:34:41 +0200

    fts

--- fixtures.sql
+++ fixtures.sql
@@ -2,7 +2,7 @@
 
 begin;
 
-set local search_path to shca;
+set local search_path to pim;
 
 create or replace function lorem() returns text as $$
 with words(words) as (select array['lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing', 'elit', 'a', 'ac', 'accumsan', 'ad', 'aenean', 'aliquam', 'aliquet', 'ante', 'aptent', 'arcu', 'at', 'auctor', 'augue', 'bibendum', 'blandit', 'class', 'commodo', 'condimentum', 'congue', 'consequat', 'conubia', 'convallis', 'cras', 'cubilia', 'cum', 'curabitur', 'curae', 'cursus', 'dapibus', 'diam', 'dictum', 'dictumst', 'dignissim', 'dis', 'donec', 'dui', 'duis', 'egestas', 'eget', 'eleifend', 'elementum', 'enim', 'erat', 'eros', 'est', 'et', 'etiam', 'eu', 'euismod', 'facilisi', 'facilisis', 'fames', 'faucibus', 'felis', 'fermentum', 'feugiat', 'fringilla', 'fusce', 'gravida', 'habitant', 'habitasse', 'hac', 'hendrerit', 'himenaeos', 'iaculis', 'id', 'imperdiet', 'in', 'inceptos', 'integer', 'interdum', 'justo', 'lacinia', 'lacus', 'laoreet', 'lectus', 'leo', 'libero', 'ligula', 'litora', 'lobortis', 'luctus', 'maecenas', 'magna', 'magnis', 'malesuada', 'massa', 'mattis', 'mauris', 'metus', 'mi', 'molestie', 'mollis', 'montes', 'morbi', 'mus', 'nam', 'nascetur', 'natoque', 'nec', 'neque', 'netus', 'nibh', 'nisi', 'nisl', 'non', 'nostra', 'nulla', 'nullam', 'nunc', 'odio', 'orci', 'ornare', 'parturient', 'pellentesque', 'penatibus', 'per', 'pharetra', 'phasellus', 'placerat', 'platea', 'porta', 'porttitor', 'posuere', 'potenti', 'praesent', 'pretium', 'primis', 'proin', 'pulvinar', 'purus', 'quam', 'quis', 'quisque', 'rhoncus', 'ridiculus', 'risus', 'rutrum', 'sagittis', 'sapien', 'scelerisque', 'sed', 'sem', 'semper', 'senectus', 'sociis', 'sociosqu', 'sodales', 'sollicitudin', 'suscipit', 'suspendisse', 'taciti', 'tellus', 'tempor', 'tempus', 'tincidunt', 'torquent', 'tortor', 'tristique', 'turpis', 'ullamcorper', 'ultrices', 'ultricies', 'urna', 'ut', 'varius', 'vehicula', 'vel', 'velit', 'venenatis', 'vestibulum', 'vitae', 'vivamus', 'viverra', 'volutpat', 'vulputate'])
@@ -10,6 +10,17 @@ select array_to_string(
     words[greatest(1, random() * 10):greatest(array_length(words, 1), random() * 100)]
 , ' ') from words;
 $$ language sql volatile;
+
+create table sentence (
+    language text not null default 'english',
+    sentence text not null
+);
+\copy sentence(sentence) from 'sentences.txt' with (format text);
+
+alter table sentence alter language set default 'french';
+\copy sentence(sentence) from 'french.txt' with (format text);
+
+grant select on sentence to app;
 
 create or replace function sentence() returns text as $$
     select sentence from sentence tablesample bernoulli(20) limit 1
@@ -19,49 +30,48 @@ set local role app;
 -- select set_config('app.tenant', 'tenant#1', true);
 
 -- truncate family cascade;
-insert into family (tenant, family, parent)
+insert into family (family, parent)
 with recursive tree(family, parent, level) as (
     select 'family#' || i, null, 1 from generate_series(1, 5) i
     union all
     select format('%s.%s', tree.family, j), tree.family, level + 1 from generate_series(1, 3) j, tree
     where level < 2
 )
-select current_setting('app.tenant', true), family, parent from tree;
+select family, parent from tree;
 
 -- truncate attribute cascade;
-insert into attribute (tenant, attribute, type)
-select current_setting('app.tenant', true), 'attribute#' || i, 'text'
+insert into attribute (attribute, type)
+select 'attribute#' || i, 'text'
 from generate_series(1, 5) i;
 
-insert into attribute (tenant, attribute, type)
-select current_setting('app.tenant', true), 'parent attribute#' || i, 'text'
+insert into attribute (attribute, type)
+select 'parent attribute#' || i, 'text'
 from generate_series(1, 5) i;
 
 -- truncate family_has_attribute cascade;
-insert into family_has_attribute (tenant, family, attribute)
-select tenant, family, attribute
+insert into family_has_attribute (family, attribute)
+select family, attribute
 from family
 join attribute using (tenant)
 where parent is not null
-and family.tenant = current_setting('app.tenant', true)
 and attribute not like 'parent attr%';
 
-insert into family_has_attribute (tenant, family, attribute)
-select current_setting('app.tenant', true), family, attribute
+insert into family_has_attribute (family, attribute)
+select family, attribute
 from family
 join attribute using (tenant)
 where parent is null
 and attribute like 'parent attr%';
 
 -- truncate product cascade;
-insert into product (tenant, product, parent, family)
-select current_setting('app.tenant', true), format('product#%s of %s', i, family), null, family
+insert into product (product, parent, family)
+select format('product#%s of %s', i, family), null, family
 from generate_series(1, 3) i,
 family
 where family.parent is null;
 
-insert into product (tenant, product, parent, family)
-select current_setting('app.tenant', true), format('child %s of %s', random(), product), product, family_child.family
+insert into product (product, parent, family)
+select format('child %s of %s', random(), product), product, family_child.family
 from product
 join family using(tenant, family)
 join family family_child on family_child.parent = family.family
@@ -69,10 +79,11 @@ join family family_child on family_child.parent = family.family
 ;
 
 -- truncate product_value cascade;
-insert into product_value (tenant, product, attribute, locale, channel, language, value)
-select current_setting('app.tenant', true), product, attribute, locale, channel, 'simple', to_jsonb(sentence())
+insert into product_value (product, attribute, locale, channel, language, value)
+select product, attribute, locale, channel, language::regconfig, to_jsonb(sentence)
 from (values ('en_EN'), ('de_DE')) locale (locale),
 (values ('ecommerce'), ('print')) channel (channel),
+sentence tablesample bernoulli(1),
 product
 join family_has_attribute using (tenant, family)
 join family using (tenant, family)
@@ -82,9 +93,10 @@ and family.parent is not null
 and attribute not like 'parent attr%';
 ;
 
-insert into product_value (tenant, product, attribute, locale, channel, language, value)
-select current_setting('app.tenant', true), product, attribute, null, null, 'simple', to_jsonb('parent data: ' || sentence())
-from product
+insert into product_value (product, attribute, locale, channel, language, value)
+select product, attribute, null, null, language::regconfig, to_jsonb('parent data: ' || sentence)
+from sentence tablesample bernoulli(1),
+product
 join family_has_attribute using (tenant, family)
 join family using (tenant, family)
 join attribute using (tenant, attribute)
--- src/schema.sql
+++ src/schema.sql
@@ -2,22 +2,22 @@
 
 begin;
 
-drop schema if exists shca cascade;
-create schema shca;
-set local search_path to shca;
+drop schema if exists pim cascade;
+create schema pim;
+set local search_path to pim;
 
 drop role if exists app;
 create role app;
-grant usage on schema shca to app;
+grant usage on schema pim to app;
 
 create domain netext as text constraint "non-empty text" check (value <> '');
 
 create table family (
-     tenant netext not null,
-     family netext not null,
-     parent netext null,
-     primary key (tenant, family),
-     foreign key (tenant, parent) references family (tenant, family) on delete cascade deferrable
+    tenant netext not null default current_setting('app.tenant', true),
+    family netext not null,
+    parent netext null,
+    primary key (tenant, family),
+    foreign key (tenant, parent) references family (tenant, family) on delete cascade deferrable
 );
 alter table family enable row level security;
 
@@ -33,14 +33,14 @@ where parent is null
 union all
 select child.tenant, child.family, child.parent, level + 1, ancestors || parent.family
 from family_ancestry parent
-join family child on (child.tenant, child.parent) = (parent.tenant, parent.family);
+join family child on child.parent = parent.family;
 
 create table category (
-     tenant netext not null,
-     category netext not null,
-     parent netext null,
-     primary key (tenant, category),
-     foreign key (tenant, parent) references category (tenant, category) on delete cascade deferrable
+    tenant netext not null default current_setting('app.tenant', true),
+    category netext not null,
+    parent netext null,
+    primary key (tenant, category),
+    foreign key (tenant, parent) references category (tenant, category) on delete cascade deferrable
 );
 alter table category enable row level security;
 
@@ -49,17 +49,17 @@ on category
 to app
 using (tenant = current_setting('app.tenant', true));
 
-create recursive view category_ancestry (category, level, ancestors) with (security_invoker) as
-select category, 1, '{}'::text[]
+create recursive view category_ancestry (tenant, category, level, ancestors) with (security_invoker) as
+select tenant, category, 1, '{}'::text[]
 from category
 where parent is null
 union all
-select child.category, level + 1, ancestors || parent.category
+select child.tenant, child.category, level + 1, ancestors || child.parent
 from category_ancestry parent
 join category child on child.parent = parent.category;
 
 create table attribute (
-    tenant netext not null,
+    tenant netext not null default current_setting('app.tenant', true),
     attribute netext not null,
     type netext not null,
     primary key (tenant, attribute)
@@ -72,12 +72,12 @@ to app
 using (tenant = current_setting('app.tenant', true));
 
 create table family_has_attribute (
-     tenant netext not null,
-     family netext not null,
-     attribute netext not null,
-     primary key (tenant, family, attribute),
-     foreign key (tenant, family) references family (tenant, family) on delete cascade,
-     foreign key (tenant, attribute) references attribute (tenant, attribute) on delete cascade
+    tenant netext not null default current_setting('app.tenant', true),
+    family netext not null,
+    attribute netext not null,
+    primary key (tenant, family, attribute),
+    foreign key (tenant, family) references family (tenant, family) on delete cascade,
+    foreign key (tenant, attribute) references attribute (tenant, attribute) on delete cascade
 );
 alter table family_has_attribute enable row level security;
 
@@ -96,16 +96,17 @@ for insert
 to app
 with check (
     not exists(
-        select from family_has_attribute ancestor
+        select from family_has_attribute ancestor_has_attribute
         join family_ancestry f using (tenant)
         where family_has_attribute.family = f.family
-        and ancestor.family = any(f.ancestors)
-        and ancestor.attribute = family_has_attribute.attribute
+        and ancestor_has_attribute.family = any(f.ancestors)
+        and ancestor_has_attribute.attribute = family_has_attribute.attribute
     )
 );
+revoke update on family_has_attribute from app;
 
 create table product (
-    tenant netext not null,
+    tenant netext not null default current_setting('app.tenant', true),
     product netext not null,
     parent netext null,
     family netext not null,
@@ -120,13 +121,22 @@ on product
 to app
 using (tenant = current_setting('app.tenant', true));
 
+create recursive view product_ancestry (tenant, product, parent, family, level, ancestors) with (security_invoker) as
+select tenant, product, parent, family, 1, '{}'::text[]
+from product
+where parent is null
+union all
+select child.tenant, child.product, child.parent, child.family, level + 1, ancestors || child.parent
+from product_ancestry parent
+join product child on child.parent = parent.product;
+
 create table product_in_category (
-     tenant netext not null,
-     product netext not null,
-     category netext not null,
-     primary key (tenant, product, category),
-     foreign key (tenant, product) references product (tenant, product) on delete cascade,
-     foreign key (tenant, category) references category (tenant, category) on delete cascade
+    tenant netext not null default current_setting('app.tenant', true),
+    product netext not null,
+    category netext not null,
+    primary key (tenant, product, category),
+    foreign key (tenant, product) references product (tenant, product) on delete cascade,
+    foreign key (tenant, category) references category (tenant, category) on delete cascade
 );
 alter table product_in_category enable row level security;
 
@@ -136,12 +146,12 @@ to app
 using (tenant = current_setting('app.tenant', true));
 
 create table product_value (
-    tenant netext not null,
+    tenant netext not null default current_setting('app.tenant', true),
     product netext not null,
     attribute netext not null,
     locale netext null,
     channel netext null,
-    language netext null,
+    language regconfig null,
     value jsonb null,
     -- primary key (tenant, product, attribute, locale, channel), -- can't do, forces not null :/
     unique nulls not distinct (tenant, product, attribute, locale, channel),
@@ -162,34 +172,18 @@ language sql strict immutable;
 create index product_value_fts
 on product_value
 using gin
-(localized_tsvector(language, value))
-where jsonb_typeof(value) = 'string';
-
-create view inherited_product_value (tenant, product, attribute, locale, channel, language, value, path) with (security_invoker) as
-with recursive product_ancestry (tenant, product, parent, path) as (
-    select tenant, product, parent, array[product]
-    from product
-    where parent is null
-    union all
-    select child.tenant, child.product, child.parent, path || child.product
-    from product_ancestry parent
-    join product child on child.parent = parent.product
-)
--- cycle tenant, product set is_cycle using path
--- search depth first by tenant, product set ordercol
-select tenant, value.product, attribute, locale, channel, language, value, path
-from product_ancestry
-join product_value value using (tenant)
-where value.product = any(path)
+(to_tsvector(language, value))
+where jsonb_typeof(value) = 'string'
+and language is not null
 ;
 
-create table sentence (
-    language text not null default 'english',
-    sentence text not null
-);
-\copy sentence(sentence) from 'sentences.sql' with (format text);
+create view inherited_product_value (tenant, product, attribute, locale, channel, language, value, ancestors) with (security_invoker) as
+select value.tenant, value.product, attribute, locale, channel, language, value, ancestors || pa.product
+from product_ancestry pa
+join product_value value using (tenant)
+where value.product = any(ancestors || pa.product)
+;
 
-
-grant all on all tables in schema shca to app;
+grant all on all tables in schema pim to app;
 
 commit;
