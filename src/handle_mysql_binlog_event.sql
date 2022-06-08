dd153e36492dcac499c2f7af7c173657120e6124
Florian Klein <florian.klein@free.fr>
2022-06-08 14:13:06 +0200

    nwip

--- src/handle_mysql_binlog_event.sql
+++ src/handle_mysql_binlog_event.sql
@@ -20,27 +20,6 @@ create table event (
     primary key (tenant, xid, index_)
 );
 
-create function consume_queue(tenants text[] default null, batch_size int default 10) returns setof event
-language sql volatile
-set search_path to "mysql binlog"
-as $$
-with inflight as (
-  select tenant, xid, index_ from event
-  where case when tenants is null then true else tenant = any(tenants) end
-  and not handled
-  order by 1, 2, 3
-  for update skip locked -- serializable? ok by tenant
-  limit batch_size
-)
--- delete from event
--- using inflight i
-update event
-set handled = true
-from inflight i
-where (event.tenant, event.xid, event.index_) = (i.tenant, i.xid, i.index_)
-returning event.*
-$$;
-
 create type mysql_product as (
     id bigint,
     parent bigint,
@@ -48,28 +27,25 @@ create type mysql_product as (
     raw_values text
 );
 
-create function handle_mysql_event(
-    inout event event
-)
+create function handle_mysql_event(inout event event)
 language plpgsql
 set search_path to "mysql binlog", pim
 as $$ begin
     case
         when event.table_ = 'product' and event.name = 'writerows' then
-            with mysql_product as (
+            insert into product (tenant, product, parent, family)
+            select event.tenant, id, null, family
+            from jsonb_populate_recordset(null::mysql_product, event.rows) r
+            on conflict on constraint product_pkey do update
+                set parent = excluded.parent,
+                family = excluded.family
+            ;
+
+            with to_insert as (
                 select * from jsonb_populate_recordset(null::mysql_product, event.rows) r
             ),
-            inserted_product as (
-                insert into product (tenant, product, parent, family)
-                select event.tenant, id, null, family
-                from mysql_product
-                on conflict on constraint product_pkey do update
-                    set parent = excluded.parent,
-                    family = excluded.family
-                returning *
-            ),
             by_attr (id, attribute, rest) as (
-              select id, j.* from mysql_product, jsonb_each(raw_values::jsonb) j
+              select id, j.* from to_insert, jsonb_each(raw_values::jsonb) j
             ),
             by_channel (id, attribute, channel, rest) as (
               select id, attribute,
@@ -84,22 +60,82 @@ as $$ begin
               from by_channel, jsonb_each(rest) j
             )
             insert into product_value (tenant, product, attribute, locale, channel, language, value)
-            select event.tenant, id, attribute, locale, channel, 'simple', value
-            from by_locale;
+            select event.tenant, public.notice(id), attribute, locale, channel, 'simple', value
+            from by_locale
+            on conflict on constraint product_value_tenant_product_attribute_locale_channel_key
+            do update
+                set locale = excluded.locale,
+                channel = excluded.channel,
+                language = excluded.language,
+                value = excluded.value
+            ;
 
         when event.table_ = 'product' and event.name = 'updaterows' then
-            update product
-            set product = after.id::text, parent = after.parent, family = after.family
-            from jsonb_array_elements(event.rows) r,
-            jsonb_populate_record(null::mysql_product, r->'after') after,
-            jsonb_populate_record(null::mysql_product, r->'before') before
-            where tenant = event.tenant
-            and product = before.id::text;
+            with to_update as (
+                select before, after
+                from jsonb_array_elements(event.rows) r,
+                jsonb_populate_record(null::mysql_product, r->'before') before,
+                jsonb_populate_record(null::mysql_product, r->'after') after
+            ),
+            updated_product as (
+                update product
+                set product = (after).id::text, parent = (after).parent, family = (after).family
+                from to_update
+                where tenant = event.tenant
+                and product = (before).id::text
+                returning *
+            ),
+            by_attr (id, attribute, rest) as (
+              select (before).id::text, j.* from updated_product, jsonb_each((after).raw_values::jsonb) j
+            ),
+            by_channel (id, attribute, channel, rest) as (
+              select id, attribute,
+                  case j.key when '<all_channels>' then null else j.key end,
+                  j.value
+              from by_attr, jsonb_each(rest) j
+            ),
+            by_locale (id, attribute, channel, locale, value) as (
+              select id, attribute, channel,
+                  case j.key when '<all_locales>' then null else j.key end,
+                  j.value
+              from by_channel, jsonb_each(rest) j
+            )
+            insert into product_value (tenant, product, attribute, locale, channel, language, value)
+            select event.tenant, id, attribute, locale, channel, 'simple', public.notice(value)
+            from by_locale
+            on conflict on constraint product_value_tenant_product_attribute_locale_channel_key
+            do update
+                set locale = excluded.locale,
+                channel = excluded.channel,
+                language = excluded.language,
+                value = excluded.value
+            ;
 
         else raise notice 'UNSUPPORTED';
     end case;
 exception when others then
     raise warning 'error %', sqlerrm;
 end $$;
+
+create function consume_queue(tenants text[] default null, batch_size int default 10) returns setof event
+language sql volatile
+set search_path to "mysql binlog"
+as $$
+with inflight as (
+  select tenant, xid, index_ from event
+  where case when tenants is null then true else tenant = any(tenants) end
+  and not handled
+  order by 1, 2, 3
+  for update -- skip locked -- serializable? ok by tenant
+  limit batch_size
+)
+-- delete from event
+-- using inflight i
+update event
+set handled = true
+from inflight i
+where (event.tenant, event.xid, event.index_) = (i.tenant, i.xid, i.index_)
+returning (handle_mysql_event(event)).*
+$$;
 
 commit;
